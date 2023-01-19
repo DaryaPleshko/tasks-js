02326dfde7ca438ca2f0b85bf068ea16b5b190e1
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-19 20:44:11 +0300

    commit

--- functions_4/8/app.js
+++ functions_4/8/app.js
@@ -1,3 +1,11 @@
 //Напишите функцию, принимающую в качестве параметра строку. Необходимо
 // оставить в строке только униакльные символы. IIFE
 // Позволяеткопироватьтекстиз -> позвляеткираьс
+
+const iife = (function (str) {
+    let newStr = ``;
+    for (let i = 0; i < str.length; i++) if (!newStr.includes(str[i])) newStr += str[i];
+    return newStr;
+}(`hellloooo`));
+
+console.log(iife)
