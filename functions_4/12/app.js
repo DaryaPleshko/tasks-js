b72cf39a40ae41e6c160144d754720a214cf3c8a
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-19 21:22:17 +0300

    commit

--- functions_4/12/app.js
+++ functions_4/12/app.js
@@ -1,2 +1,16 @@
 // Напишите функцию, принимающую в качестве параметра статичный объект.
 // Функция возвращает количество пар ключ / значение. IIFE
+
+const iife = (function (obj) {
+    count = 0;
+    for (let key in obj) (key) ? count++ : null;
+    return count;
+}({
+    id: 1,
+    name: 'dasha',
+    years: 18,
+    city: 'Minsk',
+    university: 'BSUIR'
+}));
+
+console.log(iife);
