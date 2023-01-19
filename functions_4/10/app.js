bbe0c6f6e5f05c569d52550f2e2dd4cf8decba38
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-19 21:16:08 +0300

    commit

--- functions_4/10/app.js
+++ functions_4/10/app.js
@@ -1,3 +1,15 @@
 //Напишите функцию, принимающую в качестве параметра статичный объект.
 // Функция возвращает новый объект, где значения – исключительно числа
 // первоначального объекта. IIFE
+
+const iife = (function (obj) {
+    let newObj = {};
+    for (let key in obj) (!isNaN(obj[key])) ? newObj[key] = obj[key] : null;
+    return newObj;
+}({
+    id: 1,
+    name: 'dasha',
+    years: 18
+}));
+
+console.log(iife);
