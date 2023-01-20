1753c71c9c74b2dfa02990048ae79e622fdb0c57
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-20 18:34:43 +0300

    commit

--- functions_4/13/app.js
+++ functions_4/13/app.js
@@ -1,3 +1,11 @@
 // Напишите функцию, которая принимает два параметра: строку и букву. Функция
 // должна подсчитывать количество вхождений указанной буквы в строке. Добавить
 // проверки
+
+const findLetter = (str, letter) => {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) (str[i] === letter) ? count++ : null;
+    return (`В строке ${str} - ${count} буква(ы) ${letter}`)
+}
+
+console.log(findLetter(`hanna`, `a`));
--- functions_4/15/app.js
+++ functions_4/15/app.js
@@ -1,2 +1,10 @@
 //Вычислите сумму массива целых чисел статичного массива используя рекурсию
 
+// const checkArray = (array) => {
+//     let newArray;
+//     for (let i = 0; i < array.length; i++) (Number.isInteger(array[i])) ? newArray += array[i] : null;
+
+//     if (array.length === 0) return 0;
+//     else return checkArray(array - 1);
+// }
+// console.log(checkArray([32, 54.4, 23.5, 75, 45]));
