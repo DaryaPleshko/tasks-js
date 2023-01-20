2fed6c0e758912972d6447d03c9ea9d369ada68d
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-20 18:43:28 +0300

    commit

--- functions_4/11/app.js
+++ functions_4/11/app.js
@@ -1,3 +1,24 @@
 // Работа с замыканием. Напишите функцию, каждый вызов который будет
 // генерировать случайные числа от 1 до 100, но так, чтобы они не повторялись, пока
 // не будут перебраны все числа из этого промежутка.
+
+function random() {
+    return () => {
+        let randomNumber = Math.ceil(Math.random() * 100);
+        
+        console.log(randomNumber);
+    }
+}
+
+const zamikanie = random();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
+zamikanie();
