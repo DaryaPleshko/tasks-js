f193d051e506c25fb81e41f990180abff3869ebd
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-20 13:43:51 +0300

    commit

--- functions_4/16/app.js
+++ functions_4/16/app.js
@@ -1,1 +1,7 @@
 // Вычислить факториал числа используя рекурсию 
+
+const factorial = (number) => {
+    if (number === 0) return 1;
+    else return number * factorial(number - 1);
+}
+console.log(factorial(7));
