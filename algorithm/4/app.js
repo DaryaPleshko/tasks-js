a3e78ad2cba5a2d3c5dff19f719dd76688e647b0
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-22 15:37:06 +0300

    commit

--- algorithm/4/app.js
+++ algorithm/4/app.js
@@ -1,1 +1,11 @@
 //Вычислите сумму массива целых чисел статичного массива используя рекурсию
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function sumArray(array) {
+    if (array.length === 0) return 0;
+    const lastItem = array.pop();
+    return sumArray(array) + lastItem;
+}
+
+console.log(sumArray(array));
