2fbd925882f4280c44c97502eba11b82ef1e77ea
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-01-28 23:04:34 +0300

    commit

--- error/7/app.js
+++ error/7/app.js
@@ -1,3 +1,21 @@
 //Реализуйте функцию, которая принимает число и возвращает возвенную в
 // квадрат каждую цифру числа соединяя их. Добавить проверки
 // 9119 -> 811181 (9^2 === 81, 1^2=== 1)
+
+const raisingNumber = (number) => {
+    try {
+        const splitNum = number.split('');
+        let newNumber = ``;
+        let countStr = 0;
+        for (let i = 0; i < splitNum.length; i++) {
+            (isNaN(splitNum[i])) ? countStr++ : null;
+            newNumber += splitNum[i] ** 2;
+        }
+        if (countStr > 0) throw new Error(`Неккоректный ввод`);
+        return newNumber;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+console.log(raisingNumber(`9119`));
