332e0558355a38fe2b9914d597c94c9f6f4c13a4
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-02 00:03:57 +0300

    commit

--- number-system/4/app.js
+++ number-system/4/app.js
@@ -9,9 +9,7 @@ const check = (str) => {
     try {
         const splitStr = str.toLowerCase().split('');
         let newStr = ``;
-        for (let i = 0; i < splitStr.length; i++) {
-            if (!newStr.includes(splitStr[i])) newStr += splitStr[i];
-        }
+        for (let i = 0; i < splitStr.length; i++)  if (!newStr.includes(splitStr[i])) newStr += splitStr[i];
         if (newStr.length != str.length) throw new Error(`Incorrect input`);
         return true;
     } catch (error) {
--- number-system/5/app.js
+++ number-system/5/app.js
@@ -1,1 +1,20 @@
 //Реализуйте функцию, демонстрирующую работу бинарного поиска
+
+const searchBinary = (num) => {
+    let startIndex = 0,
+        endIndex = 100;
+
+    for (let i = startIndex; i < endIndex; i++) {
+        let midl = Math.floor((startIndex + endIndex) / 2);
+        if (num === midl) {
+            return `Число найдено ${midl}`;
+        } else if (num > midl) {
+            startIndex = midl;
+            console.log(`число больше ${midl}`);
+        } else if (num < midl) {
+            endIndex = midl;
+            console.log(`число меньше ${midl}`);
+        }
+    }
+}
+console.log(searchBinary(Math.round(Math.random() * 100)));
