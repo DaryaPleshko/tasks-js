85aaa5452d38aa8a92f1204c0bf16a277a102c58
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-03 17:07:40 +0300

    commit

--- number-system/10/app.js
+++ number-system/10/app.js
@@ -5,3 +5,19 @@
 // ooxx –> true
 // xooxx –> false 
 
+const checkStr = (str) => {
+    try {
+        if (str.length % 2 === 1) throw new Error(false);
+        const splitStr = str.split('');
+        let countFirstLetter = 0,
+            countSecondLetter = 0;
+        for (let i = 0; i < splitStr.length; i++) {
+
+        }
+        if (countFirstLetter != countSecondLetter) throw new Error(false);
+        return true;
+    } catch (error) {
+        return error.message;
+    }
+}
+console.log(checkStr(`xooxx`));
--- number-system/6/app.js
+++ number-system/6/app.js
@@ -5,3 +5,18 @@
 // 145263 –> 654321
 // 123456789 –> 987654321
 
+const check = (num) => {
+    if (num < 0) throw new Error(`Введите целое число`);
+    return true;
+} 
+
+const decreasing = (num) => {
+    try {
+        check(num);
+        const sortNum = num.split('').sort().reverse().join('');
+        return sortNum;
+    } catch (error) {
+        return error.message;
+    }
+}
+console.log(decreasing(`145263`));
