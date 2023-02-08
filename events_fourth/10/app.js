45fcf72425215607d6c51bc6b2e1e08a2e30d3ac
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-08 13:32:03 +0300

    commit

--- events_fourth/10/app.js
+++ events_fourth/10/app.js
@@ -1,3 +1,10 @@
 // После нажатия на кнопку «Разблокировать» мы можем вносить изменения в
 // инпут. После нажатия на заблокировать данное действие запрещается, тем самым
 // выключая возможность редактирования значения инпута
+
+const btnFirst = document.querySelector('.btn-first'),
+    btnSecond = document.querySelector('.btn-second'),
+    inp = document.querySelector('.inp');
+
+btnFirst.addEventListener('click', () => inp.disabled = true);
+btnSecond.addEventListener('click', () => inp.disabled = false);
--- events_fourth/9/app.js
+++ events_fourth/9/app.js
@@ -1,3 +1,14 @@
 // У вас есть кнопка. После нажатия поменять background.
 
- 
+const btn = document.querySelector('.btn');
+
+let flag = true;
+btn.addEventListener('click', () => {
+    if (flag) {
+        btn.style = 'background-color:red';
+        flag = false;
+    } else {
+        btn.style = 'background-color:purple';
+        flag = true;
+    }
+})
