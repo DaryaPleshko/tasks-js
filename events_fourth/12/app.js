ae9ed5d8a78c219f63cbc0600f9d6b94b7ed0487
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-08 14:35:00 +0300

    commit

--- events_fourth/12/app.js
+++ events_fourth/12/app.js
@@ -1,2 +1,22 @@
 // Вывести в строчку все вводимые значения через инпут в виде массива. Добавить
 // проверку, что поле не пусто
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+const checkOfInp = (inp) => {
+    if (inp.value.length === 0) throw new Error('Вы ничего не ввели');
+    return true;
+}
+
+let inpValue = [];
+btn.addEventListener('click', () => {
+    try {
+        const inp = document.querySelector('.inp');
+        checkOfInp(inp);
+            inpValue.push(inp.value)
+            result.innerHTML = inpValue;
+            inp.value = '';
+    } catch (error) {
+        result.innerHTML = error.message;
+    }
+});
