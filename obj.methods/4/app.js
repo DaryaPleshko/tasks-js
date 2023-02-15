20b06002fb31e45dbf94a6d5a7b329dd9a132eba
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-15 22:10:34 +0300

    commit

--- obj.methods/4/app.js
+++ obj.methods/4/app.js
@@ -1,10 +1,15 @@
 // На входе объект. Ключи и значения - автоинкремент (генерируется автоматически
 //     от 1 до n). Написать функцию на поиск в объекте только четных ключей. 
 
-const checkObj = (num) => {
+const create = (num) => {
     let obj = {};
     for (let i = 0; i < num; i++)  obj[i] = i;
     return obj;
 };
 
-console.log(checkObj(5));
+const findValue = (obj) => {
+    const arrayKey = Object.keys(obj);
+    let chetNum = arrayKey.filter((el) => el % 2 === 0);
+    return chetNum;
+}
+console.log(findValue(create(5)));
