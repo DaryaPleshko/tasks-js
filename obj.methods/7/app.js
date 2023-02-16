fb93a673f4a183633bad8f39f8f81d7ce0892b7f
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-16 14:14:22 +0300

    commit

--- obj.methods/7/app.js
+++ obj.methods/7/app.js
@@ -2,3 +2,10 @@
 // индекса и вычислить сумму.
 // 12345 = 1^0+2^1+3^2+4^3+5^4
 
+const exponentiationNum = (num) => {
+    const splitNum = num.split('')
+    let newNum = 0;
+    for (let i = 0; i < splitNum.length; i++)  newNum += splitNum[i] ** i;
+    return newNum;
+}
+console.log(exponentiationNum(`12345`));
