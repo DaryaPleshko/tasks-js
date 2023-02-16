f6b8ab923e536e1dad6be266973b5a37c0e248df
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-16 13:56:33 +0300

    commit

--- obj.methods/5/app.js
+++ obj.methods/5/app.js
@@ -1,3 +1,12 @@
 // На входе объект и число n, символизирующее количество пар ключ-значение.
 // Ключи и значения - автоинкремент (генерируется автоматически от 1 до n).
 // Проверить есть ли ключ 10 в объекте. 
+
+const create = (num) => {
+    let obj = {};
+    for (let i = 0; i < num; i++)  obj[i] = i;
+    return obj;
+};
+const findKey = (obj) => (obj.hasOwnProperty('10')) ? true : false;
+
+console.log(findKey(create(4)));
--- obj.methods/6/app.js
+++ obj.methods/6/app.js
@@ -2,3 +2,15 @@
 // Ключи - автоинкремент (генерируется автоматически от 1 до n). Значение каждого
 // ключа – рандомное число от 1 до 100. Вывести каждое значение объекта с
 // помощью values
+
+const createObj = (num) => {
+    let obj = {};
+    for (let i = 0; i < num; i++)  obj[i] = Math.floor(Math.random() * 100);
+    return obj;
+};
+const findValues = (obj) => {
+    const arrayValues = Object.values(obj);
+    return arrayValues;
+};
+
+console.log(findValues(createObj(4)));
