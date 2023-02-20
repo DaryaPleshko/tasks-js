a081e34c94167e85227667102dd82ceb0a7f215e
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-20 20:56:34 +0300

    commit

--- prototype/8/app.js
+++ prototype/8/app.js
@@ -1,2 +1,19 @@
 // На вход программе подается число в двоичной системе счисления. Необходимо
 // преобразовать его в десятичную систему счисления
+
+const numberSystem = (numInSecondSystem) => {
+    try {
+        const splitNum = numInSecondSystem.split('').reverse();
+        let countStr = 0,
+            numInTenthSystem = 0;
+        for (let i = 0; i < splitNum.length; i++) {
+            if (isNaN(splitNum[i])) countStr++;
+            else numInTenthSystem += splitNum[i] * (2 ** i);
+        }
+        if (countStr > 0) throw new Error(`Некорректный ввод`);
+        return numInTenthSystem;
+    } catch (error) {
+        return error.message;
+    }
+};
+console.log(numberSystem(`010011`));
--- prototype/9/app.js
+++ prototype/9/app.js
@@ -1,2 +1,10 @@
 // На входе объект. Ключи и значения - автоинкремент (генерируется автоматически
 //     от 1 до n). Необходимо вывести количество пар ключ значение объекта)
+
+const checkKey = (obj) => Object.entries(obj).length;
+console.log(checkKey({
+    id: 1,
+    name: 'Darya',
+    surname: 'Pleshko',
+    city: 'Minsk'
+}))
