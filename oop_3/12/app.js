8f63142b7b91783c340c389f3c794acf5c07c7d9
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-23 17:12:46 +0300

    commit

--- oop_3/12/app.js
+++ oop_3/12/app.js
@@ -2,3 +2,25 @@
 // массива. На основании числа формируется динамический массив из n элементов
 // внутри класса. Создать функцию doHashtag, заполняющую новый массив из
 // элементов, каждое значение которого имеет вид #name
+
+class Hashtag {
+    checkArray = (array) => {
+        let number = 0;
+        for (let el of array) (!isNaN(el)) ? number += 1 : null;
+        return (number === 0) ? true : false;
+    }
+    doHashtag = (n) => {
+        try {
+            if (isNaN(n)) throw new Error(`Некорректный ввод!`);
+            let array = [];
+            for (let i = 0; i < n; i++) array.push(prompt(``));
+            this.checkArray(array);
+            const added = array.map((el) => `#${el}`);
+            return added;
+        } catch (error) {
+            return error.message;
+        }
+    }
+}
+const hashtag = new Hashtag();
+console.log(hashtag.doHashtag(6));
--- oop_3/14/app.js
+++ oop_3/14/app.js
@@ -15,3 +15,41 @@
 // Необходимо найти id клиента в массиве БД. Если совпадение есть, произвести
 // обновление значений для соответствующих ключей.
 // Если совпадения по id нет – ошибка. Добавить проверки 
+
+class ServerPut {
+    middleware(obj) {
+        if (obj.length === 0) throw new Error(`Объект пустой`);
+        return this.controller(obj);
+    }
+
+    controller(obj) {
+        try {
+            return this.service(obj);
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    service(obj) {
+        return this.repository(obj);
+    }
+
+    repository(obj) {
+        let array = [
+            { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+            { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+            { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+        ];
+        const filtered = array.filter((el) => el.id === obj.id);
+        if (filtered.length === 0) throw new Error('id не существует');
+        let bd = array.filter(el => el.id != obj.id);
+        bd.push(obj);
+        array = bd;
+        return array;
+    }
+}
+let obj = { "id": "javascript", "label": "hihi", "category": "programmingLanguages", "priority": 1 };
+const serverPut = new ServerPut();
+console.log(serverPut.middleware(obj));
