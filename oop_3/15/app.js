9c07737f909264607b3c14d7976fbb12e4ace620
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-24 10:20:03 +0300

    commit

--- oop_3/15/app.js
+++ oop_3/15/app.js
@@ -14,3 +14,39 @@
 // }`
 // Необходимо осуществить удаление по id. Если совпадения нет – ошибка. Добавить
 // проверки
+
+class ServerDelete {
+    middleware = (obj) => {
+        if (obj.length === 0) throw new Error(`Объект пустой`);
+        return this.controller(obj);
+    }
+
+    controller = (obj) => {
+        try {
+            return this.service(obj);
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    service = (obj) => {
+        return this.repository(obj);
+    }
+
+    repository = (obj) => {
+        let array = [
+            { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+            { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+            { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+        ];
+        const filtered = array.filter((el) => el.id === obj.id);
+        if (filtered.length === 0) throw new Error('id не существует');
+        array = array.filter(el => el.id != obj.id);
+        return array;
+    }
+}
+let obj = { "id": "javascript" };
+const serverDelete = new ServerDelete();
+console.log(serverDelete.middleware(obj));
