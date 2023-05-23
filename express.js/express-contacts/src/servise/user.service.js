2442b68e99d6d8d2fb3747ffbf66c3e454685bce
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-05-23 22:14:51 +0300

    commit

--- express.js/express-contacts/src/servise/user.service.js
+++ express.js/express-contacts/src/servise/user.service.js
@@ -4,7 +4,7 @@ const getAllData = async () => {
   const data = await getAllDataDb();
   if (!data.length) throw new Error('Array is empty');
   return data;
-};
+};        
 
 const getDataById = async id => {
   const data = await getDataByIdDb(id);
