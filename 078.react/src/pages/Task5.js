801308886d9209e1dc1b53845720919351fc141d
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-09-03 22:33:58 +0300

    commit

---
+++ 078.react/src/pages/Task5.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Task5() {
+    const [input, setInput] = useState();
+
+    const setInputVal = (event) => {
+        setInput(event.target.value);
+    }
+
+    return (
+        <div>
+            <p>{input}</p>
+            <input onChange={setInputVal} />
+        </div>
+    )
+}
+export default Task5;
