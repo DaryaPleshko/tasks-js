bbfe517507be23eb10251fbeb91fca3bd1b047a5
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-01-15 23:53:22 +0300

    commit

--- function/exceptions/2/app.js
+++ function/exceptions/2/app.js
@@ -1,17 +1,46 @@
 //У вас есть какае-то значение. Необходимо обработать это значение. Если это число и оно не является четным, то бросить исключение 
 
-const anyValue = 75;
-const checkIsNaNOfValue = (value) => {
-    if (isNaN(value)) throw new Error(`Это строка`);
-    return anyValue;
-};
-const checkNumOfValue = (value) => {
+const checkValue = (value) => {
     try {
-        let number = checkIsNaNOfValue(value);
-        if (number % 2 === 0) return value;
-        else throw new Error(`нечет`);
-    } catch (err) {
-        return err;
+        if (isNaN(value)) throw new Error('This is a string');
+        if (value % 2 === 1) throw new Error('ne chetnoe');
+        return true;
+    } catch (error) {
+        return error.message;
     }
 }
-console.log(checkNumOfValue(anyValue));
+
+console.log(checkValue(9));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const anyValue = 75;
+// const checkIsNaNOfValue = (value) => {
+//     if (isNaN(value)) throw new Error(`Это строка`);
+//     return anyValue;
+// };
+// const checkNumOfValue = (value) => {
+//     try {
+//         let number = checkIsNaNOfValue(value);
+//         if (number % 2 === 0) return value;
+//         else throw new Error(`нечет`);
+//     } catch (err) {
+//         return err;
+//     }
+// }
+// console.log(checkNumOfValue(anyValue));
--- function/exceptions/4/app.js
+++ function/exceptions/4/app.js
@@ -1,24 +1,52 @@
-//Дан массив внутри строки '[1,2,3,4,5]‘. Необходимо строку преобразовать в массив (JSON.parse). Если же после того как вы спарсили данные у вас 
+//Дан массив внутри строки '[1,2,3,4,5]‘. Необходимо строку преобразовать в массив (JSON.parse). 
+// Если же после того как вы спарсили данные у вас 
 //не тип данных массив, то исключение. Далее вывести только те числа, которые кратны 2
 
-const dataParse = JSON.parse(`[1, 2, 3, 4, 5]`);
-let isArray = (dParse) => {
-    if (typeof dParse === `object`) return true;
-    else throw new Error(`не массив`);
-};
-let isArrayOfNumber = (array) => {
-    let countOfError = 0;
-    array.forEach(element => (!isNaN(element)) ? true : countOfError += 1);
-    if (countOfError > 0) throw new Error(`Внутри массива есть строка`)
-    else return true;
-};
-let findEvenNumbers = (array) => {
+
+const array = JSON.parse('[1,2,3,4,5]');
+
+const checkType = (array) => {
+    if (!Array.isArray(array)) throw new Error('type error');
+    return true;
+}
+
+const findChetNum = (array) => {
     try {
-        let boolIsArray = isArray(array);
-        let boolIsArrayOfNumber = isArrayOfNumber(array);
-        if (boolIsArray === true && boolIsArrayOfNumber === true) return array.filter((element) => element % 2 == 0);
-    } catch (err) {
-        return err;
+
+    } catch (error) {
+        return error.message;
     }
-};
-console.log(findEvenNumbers(dataParse));
+}
+console.log(findChetNum(checkType(array)));
+
+
+
+
+
+
+
+
+
+
+
+// const dataParse = JSON.parse(`[1, 2, 3, 4, 5]`);
+// let isArray = (dParse) => {
+//     if (typeof dParse === `object`) return true;
+//     else throw new Error(`не массив`);
+// };
+// let isArrayOfNumber = (array) => {
+//     let countOfError = 0;
+//     array.forEach(element => (!isNaN(element)) ? true : countOfError += 1);
+//     if (countOfError > 0) throw new Error(`Внутри массива есть строка`)
+//     else return true;
+// };
+// let findEvenNumbers = (array) => {
+//     try {
+//         let boolIsArray = isArray(array);
+//         let boolIsArrayOfNumber = isArrayOfNumber(array);
+//         if (boolIsArray === true && boolIsArrayOfNumber === true) return array.filter((element) => element % 2 == 0);
+//     } catch (err) {
+//         return err;
+//     }
+// };
+// console.log(findEvenNumbers(dataParse));
